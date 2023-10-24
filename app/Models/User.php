76b9f920ffb34a2793cd46fa483c09a2f8a71441
xiaao <xiaaohehe@foxmail.com>
2023-10-24 18:46:50 +0800

    用户显示页面

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function gravatar($size = '100')
+    {
+        return "https://github.githubassets.com/images/modules/site/universe/eyebrow-23@2x.png";
+    }
 }
