b8b4a5b2247ed04ce796bcc1919a2e6de61ecab6
xiaao <xiaaohehe@foxmail.com>
2023-10-31 16:44:51 +0800

    登录成功后导航逻辑

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,6 +3,7 @@
   <head>
     <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script src="{{ mix('js/app.js') }}"></script>
   </head>
 
   <body>
@@ -15,5 +16,6 @@
         @include('layouts._footer')
       </div>
     </div>
+
   </body>
 </html>
